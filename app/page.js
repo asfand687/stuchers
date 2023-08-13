447caebd79072bc982df99c583607c1fbb6d0bf5
Asfand Yar <asfand@novoassociates.com>
2023-08-13 16:08:29 +0500

    added elements to the page

--- app/page.js
+++ app/page.js
@@ -4,16 +4,19 @@ import Footer from '@/components/Footer'
 import FormSection from '@/components/FormSection'
 import Hero from '@/components/Hero'
 import Navbar from '@/components/Navbar'
+import { GlobalProvider } from '@/context/GlobalState'
 
 export default function Home() {
   return (
-    <main>
-      <Navbar/>
-      <Hero/>
-      <FormSection/>
-      <FacesOfStuchers/>
-      <FAQ/>
-      <Footer/>
+    <main className="overflow-x-hidden">
+      <GlobalProvider>
+        <Navbar/>
+        <Hero/>
+        <FormSection/>
+        <FacesOfStuchers/>
+        <FAQ/>
+        <Footer/>
+      </GlobalProvider>
     </main>
   )
 }
